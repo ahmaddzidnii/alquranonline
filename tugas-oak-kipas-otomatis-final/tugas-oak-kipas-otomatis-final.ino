5a17216fb1a945086783462067ee7730461f6eee
ahmaddzidnii <zdngaming312@gmail.com>
2024-06-02 13:35:40 +0700

    docs arduino program

--- tugas-oak-kipas-otomatis-final/tugas-oak-kipas-otomatis-final.ino
+++ tugas-oak-kipas-otomatis-final/tugas-oak-kipas-otomatis-final.ino
@@ -1,14 +1,15 @@
 #include <LiquidCrystal_I2C.h> // include library LCD
 #include <DHT.h>               // include library DHT
 
+#define relay_kipas 3  // mendefinisikan pin relay kipas pada pin digital 3
+
 // Inisialisasi LCD dengan alamat 0x27 dan ukuran 16x2
 LiquidCrystal_I2C lcd(0x27, 16, 2);
 
-// Inisialisasi sensor DHT11 pada pin 2
+// Inisialisasi sensor DHT11 pada pin digital 2
 DHT dht(2, DHT11);
 
 // Definisikan pin untuk relay kipas
-int kipas = 3;
 
 void setup() {
   // Memulai komunikasi serial dengan baud rate 9600
@@ -19,10 +20,10 @@ void setup() {
   
   // Memulai LCD
   lcd.init();
-  lcd.backlight();
+  lcd.backlight(); // Menyalakan backlight LCD
   
   // Mengatur pin relay kipas sebagai output
-  pinMode(kipas, OUTPUT);
+  pinMode(relay_kipas, OUTPUT);
 }
 
 void loop() {
@@ -31,39 +32,34 @@ void loop() {
   int suhu = dht.readTemperature();
   
   // Tampilkan data suhu di LCD
-  lcd.setCursor(0,0);
+  lcd.setCursor(0,0); // Set kursor di kolom 0, baris 0
   lcd.print("Suhu:");
-  lcd.setCursor(6,0);
+  lcd.setCursor(6,0); // Set kursor di kolom 6, baris 0
   lcd.print(suhu);
-  lcd.setCursor(8,0);
+  lcd.setCursor(8,0); // Set kursor di kolom 8, baris 0
   lcd.print(".C");
 
   // Tampilkan status kipas di LCD
-  lcd.setCursor(0, 1);
+  lcd.setCursor(0, 1); // Set kursor di kolom 0, baris 1
   lcd.print("Kipas :");
-  lcd.setCursor(8, 1);
+  lcd.setCursor(8, 1); // Set kursor di kolom 8, baris 1
   
-  // Buffer untuk menyimpan string
-  char buffer[100];
+  char buffer[100]; // Buffer untuk menyimpan string
 
   if (suhu > 32) {
-    // Nyalakan kipas jika suhu di atas 32°C
-    digitalWrite(kipas, LOW);
-    lcd.print("ON ");
+    digitalWrite(kipas, LOW); // karena relay yg saya pakai adalah relay LOW LEVEL TRIGGER maka relay diberi LOW untuk menyalakan relay
+    lcd.print("ON "); 
     
-    // Format JSON dan cetak ke Serial Monitor
+    // Format JSON dan cetak ke Serial Monitor untuk keperluan komunikasi ke backend nodejs via serial port
     sprintf(buffer, "{\"suhu\":%d,\"kelembaban\":%d,\"kipas\":%s}", suhu, kelembaban, "true");
     Serial.println(buffer);
   } else {
-    // Matikan kipas jika suhu 32°C atau lebih rendah
-    digitalWrite(kipas, HIGH);
+    digitalWrite(kipas, HIGH); // karena relay yg saya pakai adalah relay LOW LEVEL TRIGGER maka relay diberi HIGH untuk mematikan relay
     lcd.print("OFF");
     
-    // Format JSON dan cetak ke Serial Monitor
+    // Format JSON dan cetak ke Serial Monitor untuk keperluan komunikasi ke backend nodejs via serial port
     sprintf(buffer, "{\"suhu\":%d,\"kelembaban\":%d,\"kipas\":%s}", suhu, kelembaban, "false");
     Serial.println(buffer);
   }
-  
-  // Tunggu sebentar sebelum loop berikutnya
   delay(1000);
 }
